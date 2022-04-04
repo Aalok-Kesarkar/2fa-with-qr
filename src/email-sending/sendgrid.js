dbf62bb035e4ddd8881d40aa86d5ce8501c3409d
Aalok-Kesarkar <201901002@vupune.ac.in>
2022-04-04 20:18:36 +0530

    removed unnecessary code of old smtp mailing function

--- src/email-sending/sendgrid.js
+++ src/email-sending/sendgrid.js
@@ -4,31 +4,11 @@ const v8 = require('v8')
 const nodemailer = require('nodemailer');
 
 const mailTransporter = nodemailer.createTransport({
-    // service: "gmail",
     host: 'smtp.office365.com',
-    // port: 465,
-    // ssl: true,
     auth: {
         user: 'aalok.temp@outlook.com',
         pass: 'hFuy&*t82hfy'
     }
-})
-
-const regularEmail2 = new Promise((resolve, reject) => { // start of promise function
-    (function () {    // funtion that accepts 3 arguments from caller of function
-        mailTransporter.sendMail({          // function to send email
-            from: "aalok.temp@outlook.com", // YOU CAN IGNORE NEXT 4 LINES
-            to: userEmail,
-            subject: emailSubject,
-            html: htmlText
-        }, (err) => {                       // callback to throw error (if any error generated)
-            if (err) {                      // if error generated, send promise reject
-                reject(`Couldn't send email: ${err}`)
-            } else {                        // if no error, send promise resolve
-                resolve(`OTP sent to ${userEmail} succesfully!`)
-            } // end of line 24
-        })  //  } is end of code from 21st line,    ) is end of code from 16th line
-    })  // } is end of code from 15th line,     ) is end of code from 15th line
 })
 
 const regularEmail = (userEmail, emailSubject, htmlText) => {
@@ -47,14 +27,6 @@ const regularEmail = (userEmail, emailSubject, htmlText) => {
         })
     })
 }
-
-// (err) => {
-//     if (err) {
-//         return console.log(`Couldn't send mail: ${err}`)
-//     } else {
-//         console.log(`OTP sent to ${userEmail} succesfully`)
-//     }
-// })
 
 const verifyLoginByQR = (userEmail, userName, imageBuffer) => {
     qrImage = v8.deserialize(imageBuffer)
