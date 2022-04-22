19a4cdab96845d30ca1c184f6fba10f32bd29055
Aalok-Kesarkar <201901002@vupune.ac.in>
2022-04-22 22:50:37 +0530

    fixed typo bug of PORT number

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 require('./db/mongoose')
 
-const PORT = process.env.AUTH_PROJECT_PORT || 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
